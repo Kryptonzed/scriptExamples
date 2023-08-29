e1286cd7ae05b78329d91d34cb22f8d23fc784f5
Klonder <klonder.dev@gmail.com>
2023-08-29 13:33:07 +0900

    CS Files for map
    Reference for unity procedural map generation

---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    bool click = false;
+    Camera MainCamera;
+
+    void Start()
+    {
+        MainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+    }
+
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (GetComponent<Rigidbody>().velocity.y == 0)
+            {
+                if (Global.ScrollSpeed < 20f)
+                {
+                    Global.ScrollSpeed += 10f * Time.deltaTime;
+                }
+                click = true;
+            }
+        }
+        else
+        {
+            if (click)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * Global.ScrollSpeed * 50f);
+            }
+
+            if (Global.ScrollSpeed > 10f)
+            {
+                Global.ScrollSpeed -= 10f * Time.deltaTime;
+            }
+            click = false;
+        }
+        MainCamera.fieldOfView = 80f + ((Global.ScrollSpeed - 10f) * 4f);
+	}
+}
